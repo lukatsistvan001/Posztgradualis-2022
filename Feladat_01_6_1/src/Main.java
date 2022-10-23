0257ef1579ecaf4dc1364dc2727a130aa5fe9605
lukatsistvan <lukatsistvan@yahoo.com>
2022-10-23 11:05:27 +0300

    Feladat szövege beleírva.

--- Feladat_01_6_1/src/Main.java
+++ Feladat_01_6_1/src/Main.java
@@ -1,5 +1,9 @@
 import java.util.Scanner;
 
+// 6.1
+// Kérjen be egy számot és írja ki, hogy helló annyiszor. (Emlékezzünk a régi szenvedős megoldásra
+// és értékeljük a haladást.)
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
