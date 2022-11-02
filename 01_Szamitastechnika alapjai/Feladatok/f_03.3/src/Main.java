f469bc0b68ef3af63f8c964a018366f36ef16d25
lukatsistvan001 <lukatsistvan@yahoo.com>
2022-11-02 19:54:55 +0200

    f_03.3 while nélkül

--- 01_Szamitastechnika alapjai/Feladatok/f_03.3/src/Main.java
+++ 01_Szamitastechnika alapjai/Feladatok/f_03.3/src/Main.java
@@ -16,22 +16,21 @@ public class Main {
         int iSugarVagyAtmero = sc.nextInt();
         sc.nextLine();
 
-        while ((iSugarVagyAtmero != 1) && (iSugarVagyAtmero != 2)) {
-            System.out.print("1 vagy 2? ");
-            iSugarVagyAtmero = sc.nextInt();
-            sc.nextLine();
-        }
-
-        if (iSugarVagyAtmero == 1) {
-            System.out.print("Add meg a kör sugarát: ");
-            int iSugar = sc.nextInt();
-            sc.nextLine();
-            System.out.print("A kör területe: " + iSugar * iSugar * Math.PI);
-        } else {
-            System.out.print("Add meg a kör átmérőjét: ");
-            int iAtmero = sc.nextInt();
-            sc.nextLine();
-            System.out.print("A kör területe: " + (iAtmero / 2) * (iAtmero / 2) * Math.PI);
+        switch (iSugarVagyAtmero) {
+            case 1:
+                System.out.print("Add meg a kör sugarát: ");
+                int iSugar = sc.nextInt();
+                sc.nextLine();
+                System.out.print("A kör területe: " + iSugar * iSugar * Math.PI);
+                break;
+            case 2:
+                System.out.print("Add meg a kör átmérőjét: ");
+                int iAtmero = sc.nextInt();
+                sc.nextLine();
+                System.out.print("A kör területe: " + (iAtmero / 2) * (iAtmero / 2) * Math.PI);
+                break;
+            default:
+                System.out.println("1 vagy 2 volt csak a lehetséges opció. Így jártál.");
         }
     }
 }
