fed96952c8561f5ec90cc4c252e17d90dd778349
lukatsistvan <lukatsistvan@yahoo.com>
2022-11-26 11:43:49 +0200

    f02_14.02 negatív számokra is

--- 02_Algoritmusok/Feladatok/f02_14.01/src/Main.java
+++ 02_Algoritmusok/Feladatok/f02_14.01/src/Main.java
@@ -5,13 +5,21 @@ művelet használatával.
 
 public class Main {
     public static void main(String[] args) {
-        double eredmeny = szorzas(0.5, 5);
+        double eredmeny = szorzas(-0.5, -5);
+        System.out.println(eredmeny);
     }
 
     private static double szorzas(double szorzando, int szorzo) {
-        if (szorzo == 1)
-            return szorzando;
-        else
-            return szorzas(szorzando, szorzo - 1) + szorzando;
+        if (szorzo<0){
+            szorzo = -szorzo;
+            szorzando = -szorzando;
+        }
+        return szorzasNemNegativSzammal(szorzando, szorzo);
+    }
+
+    private static double szorzasNemNegativSzammal(double szorzando, int szorzo) {
+        if (szorzo == 0)
+            return 0;
+        return szorzas(szorzando, szorzo - 1) + szorzando;
     }
 }
