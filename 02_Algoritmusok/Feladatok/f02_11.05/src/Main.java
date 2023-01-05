a4c58e335be70618ce8bc7e5b8660b637ee48639
lukatsistvan001 <lukatsistvan@yahoo.com>
2023-01-05 21:29:25 +0200

    f02_11.05

---
+++ 02_Algoritmusok/Feladatok/f02_11.05/src/Main.java
@@ -0,0 +1,32 @@
+/*
+Írjon egy programot, ami a parancssori argumentumaként kapott fájlból feltölt egy egész
+számokból álló tömböt. A fájl első sorában a feltöltendő tömb hossza van. Utána egy üress sor.
+Majd a tömb elemei szóközzel elválasztva.
+*/
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+        File bemenetiAdatok = Paths.get(args[0]).toFile();
+        FileReader fr = new FileReader(bemenetiAdatok);
+        Scanner sc = new Scanner(bemenetiAdatok);
+
+        int[] tomb = new int[sc.nextInt()];
+        sc.nextLine();
+        for (int i = 0; i < tomb.length; i++) {
+            tomb[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < tomb.length; i++) {
+            if (i < tomb.length - 1)
+                System.out.print(tomb[i] + ", ");
+            else
+                System.out.print(tomb[i]);
+        }
+    }
+}
