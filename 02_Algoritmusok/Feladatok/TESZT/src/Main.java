3ddb2e3e463cd97fbeff5b68f2652475308143ad
lukatsistvan001 <lukatsistvan@yahoo.com>
2023-01-05 21:30:06 +0200

    gyakorló projekt

---
+++ 02_Algoritmusok/Feladatok/TESZT/src/Main.java
@@ -0,0 +1,27 @@
+/*
+Írj egy programot ami addig kér be könyvtárneveket, ameddig abban pont nem lesz (pl x, y, a.txt),
+majd ez alapján létrehozza az aktuális könyvtárban az x/y/a.txt fájlt.
+*/
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        Path aktualisKonyvtar = Paths.get("").toAbsolutePath();
+        String ujKonyvtarNev;
+        do {
+            System.out.print("Add meg az új könvytárnevet: ");
+            ujKonyvtarNev = sc.nextLine();
+            if (!ujKonyvtarNev.contains("."))
+                aktualisKonyvtar = Files.createDirectory(aktualisKonyvtar.resolve(ujKonyvtarNev));
+            else
+                Files.createFile(aktualisKonyvtar.resolve(ujKonyvtarNev));
+        } while (!ujKonyvtarNev.contains("."));
+    }
+}
