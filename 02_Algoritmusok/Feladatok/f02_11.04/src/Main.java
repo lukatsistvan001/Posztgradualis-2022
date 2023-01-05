2e47c9839580be9cf4e39cf6ba76e2752deac6a0
lukatsistvan001 <lukatsistvan@yahoo.com>
2023-01-05 19:00:51 +0200

    f02_11.04 javítva

--- 02_Algoritmusok/Feladatok/f02_11.04/src/Main.java
+++ 02_Algoritmusok/Feladatok/f02_11.04/src/Main.java
@@ -11,26 +11,16 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Path aktualisKonyvtar = Paths.get("").toAbsolutePath();
         Scanner sc = new Scanner(System.in);
-        String bemenetiAdat;
+        Path aktualisKonyvtar = Paths.get("").toAbsolutePath();
+        String ujKonyvtarNev;
         do {
-            System.out.print("Add meg a következő bemeneti adatot: ");
-            bemenetiAdat = sc.nextLine();
-            if (!bemenetiAdat.contains("."))
-                aktualisKonyvtar = ujKonyvtarLetrehozasa(aktualisKonyvtar, bemenetiAdat);
+            System.out.print("Add meg az új könvytárnevet: ");
+            ujKonyvtarNev = sc.nextLine();
+            if (!ujKonyvtarNev.contains("."))
+                aktualisKonyvtar = Files.createDirectory(aktualisKonyvtar.resolve(ujKonyvtarNev));
             else
-                fajlLetrehozasa(aktualisKonyvtar, bemenetiAdat);
-        } while (!bemenetiAdat.contains("."));
-    }
-
-    private static void fajlLetrehozasa(Path aktualisKonyvtar, String ujFaljNev) throws IOException {
-        Files.createFile(aktualisKonyvtar.resolve(ujFaljNev));
-    }
-
-    private static Path ujKonyvtarLetrehozasa(Path aktualisKonyvtar, String ujKonyvtarNev) throws IOException {
-        Files.createDirectory(aktualisKonyvtar.resolve(ujKonyvtarNev));
-        aktualisKonyvtar = aktualisKonyvtar.resolve(ujKonyvtarNev);
-        return aktualisKonyvtar;
+                Files.createFile(aktualisKonyvtar.resolve(ujKonyvtarNev));
+        } while (!ujKonyvtarNev.contains("."));
     }
 }
